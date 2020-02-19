2d1e530c8467d2a6b988396d14940da7dcba2506
sleekcharly <c_ukasoanya@yahoo.com>
2020-02-19 12:22:11 +0100

    added slider on the index page2

--- src/components/home/Index.js
+++ src/components/home/Index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import HomeSlider from './Slider';
 
 class Home extends Component {
 
@@ -9,7 +10,7 @@ class Home extends Component {
     render() {
         return (
             <>
-                Home
+                <HomeSlider />
             </>
         )
     }
