209eb65006b3ac9eb49004e2904e08787866c128
sleekcharly <c_ukasoanya@yahoo.com>
2020-02-29 21:45:41 +0100

    heroku deploy commit

--- src/components/utils/paths.js
+++ src/components/utils/paths.js
@@ -1,4 +1,4 @@
-export const URL_SLIDES = "http://localhost:3005/slides";
-export const URL_BLOCKS = "http://localhost:3005/posts";
-export const URL_SUBS = "http://localhost:3005/subscriptions";
-export const URL_TEAMS = "http://localhost:3005/teams";
+export const URL_SLIDES = "https://cnba-json-server.herokuapp.com/slides";
+export const URL_BLOCKS = "https://cnba-json-server.herokuapp.com/posts";
+export const URL_SUBS = "https://cnba-json-server.herokuapp.com/subscriptions";
+export const URL_TEAMS = "https://cnba-json-server.herokuapp.com/teams";
